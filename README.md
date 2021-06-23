98c4d0c680ed565f1e67ffa60dce0c02f99635ff
aseelalsaqer <aseel_fawwaz@yahoo.com>
2021-06-23 17:09:07 +0300

    adding the functions by Aseel

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # About-Me
 guessing-game
+# Navigator is Ehab
+# Driver is Aseel
+**Aseel added the sixth and seventh Qustions and the functions to each qustion.**
--- js/app.js
+++ js/app.js
@@ -1,172 +1,184 @@
 'use strict';
-
-/*let name = prompt('Hi, what\'s your name')
-alert('Hi ' + name +' I would like to play a qussing game with you about me, let\'s go ')
-let fir = prompt('I\'am above 25 years old? yes or no')
-switch (fir.toLocaleLowerCase()){
-    case 'yes':
-    case 'y':
-        console.log(fir);
-        alert('correct,let\'s go to the second quastion')
-        break;
-    case 'no':    
-    case 'n':
-        console.log(fir);
-        alert('booooo,I\'m 26')
-}
+let score = 0;
+let name = prompt('Hi, what\'s your name');
+alert('Hi ' + name + ' I would like to play a qussing game with you about me, let\'s go ');
+let fir = prompt('I\'am above 25 years old? yes or no');
+function ehab1(fir) {
+    switch (fir.toLocaleLowerCase()) {
+        case 'yes':
+        case 'y':
+            console.log(fir);
+            alert('correct,let\'s go to the second quastion');
+            score++;
+            break;
+        case 'no':
+        case 'n':
+            console.log(fir);
+            alert('booooo,I\'m 26');
+    }
+} ehab1(fir);
 let sec = prompt('I\'m from jordan? yes or no').toLocaleLowerCase();
-let yes ='yes';
-let y ='y';
-if(sec.toLocaleLowerCase()==yes || sec.toLocaleLowerCase()==y) {
-    console.log(sec);
-    alert('nice, I\'m from jordn-amman');
-} else {
-    console.log(sec);
-    alert('boooooo, I live in amman-jordan');
-}
-let third = prompt('Do you think that I have a degree in programming? yes or no ')
- switch (third.toUpperCase()){
-     case 'NO':
-     case 'N':
-          console.log(third);
-          alert('correct, so far I don\'t have a degree in programming')
-          break;
-     case 'YES':
-     case 'Y': 
-     console.log(third);
-     alert('sadlly i don\'t have one yet')          
+let yes = 'yes';
+let y = 'y';
+function ehab2(sec) {
+    if (sec.toLocaleLowerCase() == yes || sec.toLocaleLowerCase() == y) {
+        console.log(sec);
+        alert('nice, I\'m from jordn-amman');
+        score++;
+    } else {
+        console.log(sec);
+        alert('boooooo, I live in amman-jordan');
     }
-let fourth = prompt('I\'m still learning about programming ? yes or no')
- switch (fourth.toLocaleLowerCase()){
-     case 'yes':
-     case 'y':
-        console.log(fourth);
-        alert('correct, I\'m still studing @ ASAC in 201 level')
-             break;
-            
-     case 'n':
-     case 'no':    
-         console.log(fourth);
-         alert('false, I\'m still a student')
-        break;
- }
- let fifth = prompt('Did you enjoy our gussing game').toLocaleLowerCase();
- if(fifth.toLocaleLowerCase() ==yes || fifth.toLocaleLowerCase()==y){
-console.log(fifth);
-alert('I\'m glade that you enjoied our game ' + name)
- } else {
-     console.log(fifth);
-     alert('Sorry to bother you ' + name)
-
- }<-- this is an infinite loop --->
- let num = 3;
- let six = prompt('guisse a number from 1-6');
- while(six > 6){
-    six = prompt('ya habibi wallah men 1-6','yalla men 1-6');}
-    while(six!=num){
- 
-     switch(six){
-         case '6':
-             six = prompt('you are high','go down')
-             console.log(six);
-             break;
-         case '5':
-             six = prompt('you are high','go down')
-             console.log(six);
-             break;
-         case '4':
-             six = prompt('you are so close','go down')
-             console.log(six);
-             break;
-         case '2':
-            six = prompt('you are so close','go up')
-            console.log(six);
+} ehab2(sec);
+let third = prompt('Do you think that I have a degree in programming? yes or no ');
+function ehab3(third) {
+    switch (third.toUpperCase()) {
+        case 'NO':
+        case 'N':
+            console.log(third);
+            alert('correct, so far I don\'t have a degree in programming');
+            score++;
             break;
-         case '1':
-             six = prompt('you picked a far low number','go up')
-             console.log(six); 
-             break;     
-
-         case '3':
-             alert('great,correct answer')
-             console.log(six);
-             break;
-
-             default:
-                 while(six > 6 || six < 1){
-                 six = prompt('from 1-6 only','play with me')
-                console.log(six);}
-
-
-     }
-    }<---- this is an infinite loop ----> */
-
-
-/*for (let i = 1; i < 4; i++){
-    let my =prompt('guess my age,you only has 4  opportunities to get the correct answer ?','hint: it\'s between 30 & 20');
-    switch(my){
-        case '30':
-            alert('you are so high');
-            my = prompt('try again','between 20 -30');
-            break;
-        case '29':   
-            alert('you are high');
-            my = prompt('try again','between 20 -30');
-            break;
-        case '28':   
-            alert('you are high');
-            my = prompt('try again','between 20 -30');
-            break;
-        case '28':   
-            alert('you are high');
-            my = prompt('try again','between 20 -30');
-            break;
-        case '27':
-            alert('you are high');
-            my = prompt('try again','between 20 -30');
-            break;
-        case '26':
-            alert('correct;I\'m 26 years old');
-            break;
-        case '25':
-            alert('you are below the correct answer');
-            my = prompt('try again','between 20 -30');
-            break;
-        case '24':
-            alert('you are below the correct answer');
-            my = prompt('try again','between 20 -30');
-            break;
-        case '23':
-            alert('you are below the correct answer');
-            my = prompt('try again','between 20 -30');
-            break;
-        case '22':
-            alert('you are below the correct answer');
-            my = prompt('try again','between 20 -30');
-            break;
-        case '21':
-            alert('you are below the correct answer');
-            my = prompt('try again','between 20 -30');
-            break;
-        case '20':
-            alert('you are far below the correct answer');
-            my = prompt('try again','between 20 -30');
-            break;                                         
+        case 'YES':
+        case 'Y':
+            console.log(third);
+            alert('sadlly i don\'t have one yet');
     }
-}*/
+} ehab3(third);
+let fourth = prompt('I\'m still learning about programming ? yes or no');
+function ehab4(fourth) {
+    switch (fourth.toLocaleLowerCase()) {
+        case 'yes':
+        case 'y':
+            console.log(fourth);
+            alert('correct, I\'m still studing @ ASAC in 201 level');
+            score++;
+            break;
+
+        case 'n':
+        case 'no':
+            console.log(fourth);
+            alert('false, I\'m still a student');
+            break;
+    }
+} ehab4(fourth);
+let fifth = prompt('Did you enjoy our gussing game').toLocaleLowerCase();
+function ehab5(fifth) {
+    if (fifth.toLocaleLowerCase() == yes || fifth.toLocaleLowerCase() == y) {
+        console.log(fifth);
+        alert('I\'m glade that you enjoied our game ' + name);
+    } else {
+        console.log(fifth);
+        alert('Sorry to bother you ' + name);
+    }
+} ehab5(fifth);
+
+    //  }<-- this is an infinite loop --->
+    //  let num = 3;
+    //  let six = prompt('guisse a number from 1-6');
+    //  while(six > 6){
+    //     six = prompt('ya habibi wallah men 1-6','yalla men 1-6');}
+    //     while(six!=num){
+
+    //      switch(six){
+    //          case '6':
+    //              six = prompt('you are high','go down')
+    //              console.log(six);
+    //              break;
+    //          case '5':
+    //              six = prompt('you are high','go down')
+    //              console.log(six);
+    //              break;
+    //          case '4':
+    //              six = prompt('you are so close','go down')
+    //              console.log(six);
+    //              break;
+    //          case '2':
+    //             six = prompt('you are so close','go up')
+    //             console.log(six);
+    //             break;
+    //          case '1':
+    //              six = prompt('you picked a far low number','go up')
+    //              console.log(six); 
+    //              break;     
+
+    //          case '3':
+    //              alert('great,correct answer')
+    //              console.log(six);
+    //              break;
+
+    //              default:
+    //                  while(six > 6 || six < 1){
+    //                  six = prompt('from 1-6 only','play with me')
+    //                 console.log(six);}
 
 
-for(let i=0;i<3;i++){
-    let six=prompt('guess my age','it\'s between 20-30');
-    if(six>26){
-        alert('too high');
-        six=prompt('try a lower number');  
-}
-    if(six<26){
-        alert('too low');
-        six=('try a higher number');
-}
-}
+    //      }
+    //     }<---- this is an infinite loop ----> 
 
 
 
+
+
+
+
+
+    let age = prompt('Guess how old I\'m ?', 'your answer should be a range like : 10-20');
+
+    while (!age) {
+        age = prompt('Enter a Number Please', 'Ex:100-110');
+    }
+    let q = 0;
+
+
+
+    function ehab6(q) {
+        while (q < 3) {
+            if (age == '20-30') {
+                alert('That\'s right');
+                score++;
+                break;
+            }
+            if (age < '20-30') {
+
+                age = prompt('Try a higher value');
+                q++
+
+            } else {
+
+                age = prompt('Try a lower value');
+                q++
+
+            }
+        }
+    } ehab6(q);
+
+    let car = ['Benz', 'BMW', 'Golf', 'bugatti']
+    let correct = false;
+    let v = '';
+    function ehab7(v) {
+        for (let k = 0; k < 6; k++) {
+            v = prompt('Guess my Favorite car ', 'Choose a German car');
+
+
+
+            for (let m = 0; m < car.length; m++) {
+
+                if (v == car[m]) {
+                    alert('Good Job');
+                    score++;
+                    correct = true
+                    k = 7;
+                    break;
+                }
+                if (correct) {
+                    break;
+                }
+            }
+
+        }
+    } ehab7(v);
+alert('My Favorite car is : BMW');
+
+
+alert('Thank you For your time ' + name + ',out of 6 your score is: ' + score);
